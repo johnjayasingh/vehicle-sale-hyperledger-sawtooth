fa56efabdd0835df28beeba2190a6247c365d4cf
Your Name <you@example.com>
2022-04-28 00:18:45 +0530

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const { TransactionProcessor } = require('sawtooth-sdk-js/processor');
+const IntegerKeyHandler = require('./handler/intkey');
+
+const VALIDATOR_URL = 'tcp://localhost:4004'
+const transactionProcessor = new TransactionProcessor(VALIDATOR_URL)
+
+// Add All the transaction processor handler
+
+transactionProcessor.addHandler(new IntegerKeyHandler());
+
+
+transactionProcessor.start();
+
+
+//Handle Stop Process
+process.on('SIGUSR2', () => {
+    transactionProcessor._handleShutdown();
+})
